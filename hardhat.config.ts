8e208c529e7478ea45ca7e12ec6181d191ab4e18
sondotpin <hec.stw@gmail.com>
2021-07-15 00:37:13 +0700

    add deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,22 @@
+require("dotenv").config({ path: './.env.dev' });
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-typechain';
 import 'hardhat-deploy';
+import '@nomiclabs/hardhat-etherscan';
 
 // You have to export an object to set up your config
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
 const config: HardhatUserConfig = {
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_ENDPOINT,
+    },
+    hardhat: {},
+  },
   solidity: {
     compilers: [
       {
